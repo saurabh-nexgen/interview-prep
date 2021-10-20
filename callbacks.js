14bc6be229e0a413703ab5204318383723eca731
saurabh-nexgen <91046377+saurabh-nexgen@users.noreply.github.com>
2021-10-20 11:01:47 +0530

    Update callbacks.js

--- callbacks.js
+++ callbacks.js
@@ -11,3 +11,8 @@ function x(y){
 x(function (){
 console.log("y");
 });
+//explanation:first of all javascript start executing code line by line when it comes to setTimeout js engine know that it cant execute that so passed it into webApI environment and attach callbacks function to it but javscript not wait for timer to be expired it moves to next line where we calling x and start executing x function so first x will be on console then y() will be called and y will be on console now callstack is empty timer would have been expired so the event loop will pass timer callback function into callstack and we will have timer expired after 5 seconds on console 
+/*output:x
+        :y
+        :timer expired after 5 seconds
+*/ 
